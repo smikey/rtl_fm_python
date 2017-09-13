f1092c6907bb52d8e609a7b02f4e0b94462826cb
Eric Kehler <smikey@smikey.de>
2017-09-13 22:18:41 +0200

    Update start_web.sh
    changed audio output for usb sound card and performance

--- start_web.sh
+++ start_web.sh
@@ -1,1 +1,4 @@
-./rtl_fm_python_web.py -M wbfm -f 101.1M - |aplay -r 32000 -f S16_LE -t raw -c 1 
+#./rtl_fm_python_web.py -M wbfm -f 101.1M - |aplay -r 32000 -f S16_LE -t raw -c 1 
+
+# used this for performance reasons and usb sound card
+./rtl_fm_python_web.py -M wbfm -f 101.1M - | aplay -r 24000 -f S16_LE -c 1 -q -V mono -D plughw:1,0
